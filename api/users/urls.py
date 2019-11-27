8d460287fcdc6124de812baa3645de94b9c1586a
Marly Emad William <marlywilliam31@gmail.com>
2019-11-27 23:01:28 +0200

    Register endpoint with username, email and password

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login
+from .views import UserCreateAPIView, login
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('login/', login, name="login")
+    path('login/', login, name="login"),
+    path('register/', UserCreateAPIView.as_view(), name="register")
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -8,7 +8,15 @@ from rest_framework.status import (
     HTTP_200_OK
 )
 from rest_framework.response import Response
+from rest_framework import generics
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 @csrf_exempt
 @api_view(["POST"])
